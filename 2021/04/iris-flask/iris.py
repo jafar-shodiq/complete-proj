b23f12d6014e1c27225297595b968a83b47a1b78
Jafar Shodiq <j.shodiq2312@gmail.com>
2021-07-21 15:54:18 +0700

    first commit

---
+++ 2021/04/iris-flask/iris.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv('iris.data')
+
+X = np.array(df.iloc[:, 0:4])
+y = np.array(df.iloc[:, 4:])
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+sv = SVC(kernel='linear').fit(X_train, y_train)
+
+pickle.dump(sv, open('iris.pkl', 'wb'))
